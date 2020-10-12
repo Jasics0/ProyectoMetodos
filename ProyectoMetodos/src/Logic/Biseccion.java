23c1004d26f6b7f5826af608d03ef65fab226a0c
SrBiolizard <72704984+SrBiolizard@users.noreply.github.com>
2020-10-11 23:06:50 -0500

    Bisecc

--- ProyectoMetodos/src/Logic/Biseccion.java
+++ ProyectoMetodos/src/Logic/Biseccion.java
@@ -5,16 +5,73 @@
  */
 package Logic;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Sebástian
  */
 public class Biseccion {
+    double a=0,b=0;
     private final Funcion funcion = new Funcion();
+    
     public Biseccion(String funcion) {
         this.funcion.definirFuncion(funcion);
         this.funcion.valorX(1);
+        while (this.funcion.getResultado().contains("error")) {
+            System.out.print("La función no sirve weonaso. Digite otra:");
+            this.funcion.definirFuncion(new Scanner(System.in).next());
+        }
     }
+       
+    public static void raizEqu(){
+        Funcion porno= new Funcion();
+        porno.definirFuncion("x^(2)-43");
+        porno.valorX(3);
+         double num1=10.2,num2=30,aux;
+         int  entero=(int)num1 ,decimal=(int)(num1%1)*10;
+             if(num1>0 && num2>0){
+                if(num1<=num2){
+                 while(num1>0) {
+                    num2=num1;
+                    if(decimal==0){
+                       decimal=9;
+                       entero--;
+                    } else{decimal--;}  
+              
+                    num1=Double.parseDouble(entero+"."+decimal);
+               
+              
+                    System.out.println("num1  :"+num1+"    num2  :"+num2);                                      
+            }
+                 num1-=0.1;          
+                 System.out.println("num1  :"+num1+"    num2  :"+num2);
+            }else{
+                  aux=num1;
+                  num1=num2;
+                  num2=aux;
+                  while(num1>0) {
+                    num2=num1;
+                    if(decimal==0){
+                       decimal=9;
+                       entero--;
+                    } else{decimal--;}  
+              
+                    num1=Double.parseDouble(entero+"."+decimal);
+               
+              
+                    System.out.println("num1  :"+num1+"    num2  :"+num2);                                      
+            }
+                 num1-=0.1;          
+                 System.out.println("num1  :"+num1+"    num2  :"+num2);
+                }    
+                
+                }
+        
+        
+    
+}    
+    
     
     
     
@@ -22,19 +79,40 @@ public class Biseccion {
     
     public static void main(String[] args) {
         
-        double num=1.5;
-        int num1=(int)num;
-        int num2=(int) ((num%1)*10);
-        double numero;
-        for (int i = 0; i < 10; i++) {
-           num2++;
-           if(num2==10){
-           num1++;
-           num2=0;
-           }
+        raizEqu();
+        
+        /* Funcion porno= new Funcion();
+        porno.definirFuncion("x^(1/5)");
+        porno.valorX(3);
+        System.out.println(porno.getResultado());
+        */
+        /* double num1=10.2,num2=30,numero1;
+        int  entero=(int)num1 ,decimal=(int)(num1%1)*10;
+        if(num1>0 && num2>0){
+            if(num1<=num2){
+                while(num1>0) {
+              num2=num1;
+              if(decimal==0){
+                  decimal=9;
+                  entero--;
+              } else{decimal--;}  
+              
+              num1=Double.parseDouble(entero+"."+decimal);
+               
+              
+            System.out.println("num1  :"+num1+"    num2  :"+num2);
+            
+             
+              
+              
+            }
+                num1-=0.1;          
+                System.out.println("num1  :"+num1+"    num2  :"+num2);
+            }
            
-            System.out.println();
-        }
+            
+                }
+        */
     }
     
 }
