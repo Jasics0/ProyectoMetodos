d4ab2140675d07d0a9f3a61d73e196c60aff7799
Javier Andres Charry <Jasics0@users.noreply.github.com>
2020-10-12 21:18:11 -0500

    Primer cohorte

--- ProyectoMetodos/src/Logic/Biseccion.java
+++ ProyectoMetodos/src/Logic/Biseccion.java
@@ -1,240 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Logic;
 
-import java.util.Scanner;
-
-/**
- *
- * @author Sebástian
- */
 public class Biseccion {
-    double a=0,b=0;
+
     private final Funcion funcion = new Funcion();
-    
+    private double xi, xu;
+
     public Biseccion(String funcion) {
         this.funcion.definirFuncion(funcion);
-        this.funcion.valorX(1);
-        while (this.funcion.getResultado().contains("error")) {
-            System.out.print("La función no sirve weonaso. Digite otra:");
-            this.funcion.definirFuncion(new Scanner(System.in).next());
+        this.xi = this.funcion.getA();
+        this.xu = this.funcion.getB();
+    }
+
+    public double biseccion() {
+        double xr=(xi+xu)/2;
+        double fBack = 0;
+        double fNext=xr ;
+        while (Float.isNaN(Float.parseFloat(((fNext - fBack) / fNext) + "")) || (Math.round(Math.abs((fNext - fBack) / fNext) * 100)) != 0) {
+            fBack=fNext;
+            if (evaluarFuncion(xi) * evaluarFuncion(fNext)<0){
+                xu=fNext;
+            } else{
+                xi=fNext;
+            }
+            fNext= (xi+xu)/2;
         }
+        return fNext;
     }
-       
-    public static void raizEqu(){
-        double x=1,y=2;
-        Funcion porno= new Funcion();
-        porno.definirFuncion("x^(3)-64");
-        porno.valorX(x);
-        Funcion porno2= new Funcion();
-        porno2.definirFuncion("x^(3)-64");
-        porno2.valorX(y);
-         double aux;
-         int  entero=(int)x ,decimal=(int)(x%1)*10;
-         double parce=Double.parseDouble(entero+"."+decimal);
-             if(Double.parseDouble(porno.getResultado())>0 && Double.parseDouble(porno2.getResultado())>0){
-                if(Double.parseDouble(porno.getResultado())<=Double.parseDouble(porno2.getResultado())){
-                 while(Double.parseDouble(porno.getResultado())>0) {
-                    y=x;
-                    
-                    if(x>=0.1){
-                    if(decimal==0){
-                       decimal=9;
-                       entero--;
-                       x=Double.parseDouble(entero+"."+decimal);
-                    } else{decimal--;
-                    x=Double.parseDouble(entero+"."+decimal);} 
-                    }else{
-                    if(x==0.0){
-                        decimal=1;
-                        entero=0;
-                        x=(Double.parseDouble(entero+"."+decimal))*-1;
-                    }else{if(decimal==10){
-                    decimal=0;
-                    entero++;
-                     x=(Double.parseDouble(entero+"."+decimal))*-1;
-                    }else{decimal++;
-                    x=(Double.parseDouble(entero+"."+decimal))*-1;}}} 
-              ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    
-               porno.valorX(x);
-               porno2.valorX(y);
-              
-                  System.out.println("num1  :??"+x+"??  "+Double.parseDouble(porno.getResultado())+"    num2  :??"+y+"??   "+Double.parseDouble(porno2.getResultado()));                                     
-            }
-                 x-=0.1;          
-                  System.out.println("num1  :??"+x+"??  "+Double.parseDouble(porno.getResultado())+"    num2  :??"+y+"??   "+Double.parseDouble(porno2.getResultado()));
-                porno.valorX(x);
-                porno2.valorX(y);
-                }else{
-                  aux=x;
-                  x=y;
-                  y=aux;
-                   porno.valorX(x);
-                porno2.valorX(y);
-                  while(Double.parseDouble(porno.getResultado())>0) {
-                    y=x;
-                    
-                    if(x>=0.1){
-                    if(decimal==0){
-                       decimal=9;
-                       entero--;
-                       x=Double.parseDouble(entero+"."+decimal);
-                    } else{decimal--;
-                    x=Double.parseDouble(entero+"."+decimal);} 
-                    }else{
-                    if(x==0.0){
-                        decimal=1;
-                        entero=0;
-                        x=(Double.parseDouble(entero+"."+decimal))*-1;
-                    }else{if(decimal==10){
-                    decimal=0;
-                    entero++;
-                     x=(Double.parseDouble(entero+"."+decimal))*-1;
-                    }else{decimal++;
-                    x=(Double.parseDouble(entero+"."+decimal))*-1;}}} 
-              ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    
-               porno.valorX(x);
-               porno2.valorX(y);
-              
-                  System.out.println("num1  :??"+x+"??  "+Double.parseDouble(porno.getResultado())+"    num2  :??"+y+"??   "+Double.parseDouble(porno2.getResultado()));                                     
-            }
-                 x-=0.1;          
-                  System.out.println("num1  :??"+x+"??  "+Double.parseDouble(porno.getResultado())+"    num2  :??"+y+"??   "+Double.parseDouble(porno2.getResultado()));
-                porno.valorX(x);
-                porno2.valorX(y);
-                
-                }    
-                
-                }if(Double.parseDouble(porno.getResultado())<0 && Double.parseDouble(porno2.getResultado())<0){
-                 if(Double.parseDouble(porno.getResultado())<=Double.parseDouble(porno2.getResultado())){
-                 while(Double.parseDouble(porno.getResultado())>0) {
-                    y=x;
-                    
-                    if(x>=0.1){
-                    if(decimal==0){
-                       decimal=9;
-                       entero--;
-                       x=Double.parseDouble(entero+"."+decimal);
-                    } else{decimal--;
-                    x=Double.parseDouble(entero+"."+decimal);} 
-                    }else{
-                    if(x==0.0){
-                        decimal=1;
-                        entero=0;
-                        x=(Double.parseDouble(entero+"."+decimal))*-1;
-                    }else{if(decimal==10){
-                    decimal=0;
-                    entero++;
-                     x=(Double.parseDouble(entero+"."+decimal))*-1;
-                    }else{decimal++;
-                    x=(Double.parseDouble(entero+"."+decimal))*-1;}}} 
-              ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    
-               porno.valorX(x);
-               porno2.valorX(y);
-              
-                  System.out.println("num1  :??"+x+"??  "+Double.parseDouble(porno.getResultado())+"    num2  :??"+y+"??   "+Double.parseDouble(porno2.getResultado()));                                     
-            }
-                 x-=0.1;          
-                  System.out.println("num1  :??"+x+"??  "+Double.parseDouble(porno.getResultado())+"    num2  :??"+y+"??   "+Double.parseDouble(porno2.getResultado()));
-                porno.valorX(x);
-                porno2.valorX(y);
-                }else{
-                  aux=x;
-                  x=y;
-                  y=aux;
-                   porno.valorX(x);
-                porno2.valorX(y);
-                  while(Double.parseDouble(porno.getResultado())>0) {
-                    y=x;
-                    
-                    if(x>=0.1){
-                    if(decimal==0){
-                       decimal=9;
-                       entero--;
-                       x=Double.parseDouble(entero+"."+decimal);
-                    } else{decimal--;
-                    x=Double.parseDouble(entero+"."+decimal);} 
-                    }else{
-                    if(x==0.0){
-                        decimal=1;
-                        entero=0;
-                        x=(Double.parseDouble(entero+"."+decimal))*-1;
-                    }else{if(decimal==10){
-                    decimal=0;
-                    entero++;
-                     x=(Double.parseDouble(entero+"."+decimal))*-1;
-                    }else{decimal++;
-                    x=(Double.parseDouble(entero+"."+decimal))*-1;}}} 
-              ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    
-               porno.valorX(x);
-               porno2.valorX(y);
-              
-                  System.out.println("num1  :??"+x+"??  "+Double.parseDouble(porno.getResultado())+"    num2  :??"+y+"??   "+Double.parseDouble(porno2.getResultado()));                                     
-            }
-                 x-=0.1;          
-                  System.out.println("num1  :??"+x+"??  "+Double.parseDouble(porno.getResultado())+"    num2  :??"+y+"??   "+Double.parseDouble(porno2.getResultado()));
-                porno.valorX(x);
-                porno2.valorX(y);
-                
-                }    
-                
-                }
-                        
-        
-    
-}    
-    
-    
-    
-    
-    
-    
+
+    private double evaluarFuncion(double x){
+        Funcion funcion = new Funcion();
+        funcion.definirFuncion(this.funcion.getFuncion());
+        funcion.valorX(x);
+        return Double.parseDouble(funcion.getResultado());
+    }
+
     public static void main(String[] args) {
-        
-        raizEqu();
-       
-        
-        
-        
-        /* Funcion porno= new Funcion();
-        porno.definirFuncion("x^(1/5)");
-        porno.valorX(3);
-        System.out.println(porno.getResultado());
-        */
-        /* double num1=10.2,num2=30,numero1;
-        int  entero=(int)num1 ,decimal=(int)(num1%1)*10;
-        if(num1>0 && num2>0){
-            if(num1<=num2){
-                while(num1>0) {
-              num2=num1;
-              if(decimal==0){
-                  decimal=9;
-                  entero--;
-              } else{decimal--;}  
-              
-              num1=Double.parseDouble(entero+"."+decimal);
-               
-              
-            System.out.println("num1  :"+num1+"    num2  :"+num2);
-            
-             
-              
-              
-            }
-                num1-=0.1;          
-                System.out.println("num1  :"+num1+"    num2  :"+num2);
-            }
-           
-            
-                }
-        */
+        Biseccion b = new Biseccion("x^2+ln(x)+e^x-205");
+        System.out.println(b.biseccion());
     }
-    
 }
